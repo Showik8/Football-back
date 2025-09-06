4dfe232cfadd05ed3f8c4e42779b27bcf95e3238
Showik8 <shotaabashidze88@gmail.com>
2025-09-06 23:08:17 +0400

    deploy

--- src/constants.ts
+++ src/constants.ts
@@ -1,170 +1,170 @@
-// const matches = [
-//   { Home: "Real Madrid", Away: "Barcelona", date: "08 October", time: "23:00" },
-//   { Home: "Real Madrid", Away: "Barcelona", date: "08 October", time: "23:00" },
-//   { Home: "Real Madrid", Away: "Barcelona", date: "08 October", time: "23:00" },
-//   { Home: "Real Madrid", Away: "Barcelona", date: "08 October", time: "23:00" },
-//   { Home: "Real Madrid", Away: "Barcelona", date: "08 October", time: "23:00" },
-// ];
+const matches = [
+  { Home: "Real Madrid", Away: "Barcelona", date: "08 October", time: "23:00" },
+  { Home: "Real Madrid", Away: "Barcelona", date: "08 October", time: "23:00" },
+  { Home: "Real Madrid", Away: "Barcelona", date: "08 October", time: "23:00" },
+  { Home: "Real Madrid", Away: "Barcelona", date: "08 October", time: "23:00" },
+  { Home: "Real Madrid", Away: "Barcelona", date: "08 October", time: "23:00" },
+];
 
-// const news = [
-//   { text: "mkasdkmasdmkads", photo_url: "https://placehold.co/250x250.png" },
-//   { text: "kmasdmakdsmkmed", photo_url: "https://placehold.co/230x170.png" },
-//   { text: "kmasdmakdsmkmed", photo_url: "https://placehold.co/230x170.png" },
-//   { text: "kmasdmakdsmkmed", photo_url: "https://placehold.co/230x170.png" },
-//   { text: "kmasdmakdsmkmed", photo_url: "https://placehold.co/230x170.png" },
-// ];
+const news = [
+  { text: "mkasdkmasdmkads", photo_url: "https://placehold.co/250x250.png" },
+  { text: "kmasdmakdsmkmed", photo_url: "https://placehold.co/230x170.png" },
+  { text: "kmasdmakdsmkmed", photo_url: "https://placehold.co/230x170.png" },
+  { text: "kmasdmakdsmkmed", photo_url: "https://placehold.co/230x170.png" },
+  { text: "kmasdmakdsmkmed", photo_url: "https://placehold.co/230x170.png" },
+];
 
-// type Player = {
-//   id: number;
-//   name: string;
-//   jersey: number;
-//   goal: number;
-//   matchPlayed?: number;
-//   assist: number;
-//   view: number;
-// };
+type Player = {
+  id: number;
+  name: string;
+  jersey: number;
+  goal: number;
+  matchPlayed?: number;
+  assist: number;
+  view: number;
+};
 
-// const players: Player[] = [
-//   {
-//     id: 1,
-//     name: "Giorgi Papava",
-//     jersey: 1,
-//     goal: 0,
-//     matchPlayed: 5,
-//     assist: 0,
-//     view: 12,
-//   },
-//   {
-//     id: 2,
-//     name: "Levan Koberidze",
-//     jersey: 2,
-//     goal: 1,
-//     matchPlayed: 7,
-//     assist: 0,
-//     view: 20,
-//   },
-//   {
-//     id: 3,
-//     name: "Nika Tchikovani",
-//     jersey: 3,
-//     goal: 0,
-//     matchPlayed: 6,
-//     assist: 2,
-//     view: 18,
-//   },
-//   {
-//     id: 4,
-//     name: "Zurab Lobjanidze",
-//     jersey: 4,
-//     goal: 2,
-//     matchPlayed: 8,
-//     assist: 1,
-//     view: 25,
-//   },
-//   {
-//     id: 5,
-//     name: "Irakli Shengelia",
-//     jersey: 5,
-//     goal: 1,
-//     matchPlayed: 4,
-//     assist: 0,
-//     view: 14,
-//   },
-//   {
-//     id: 6,
-//     name: "Saba Khutsishvili",
-//     jersey: 6,
-//     goal: 0,
-//     matchPlayed: 9,
-//     assist: 3,
-//     view: 27,
-//   },
-//   {
-//     id: 7,
-//     name: "Giorgi Kapanadze",
-//     jersey: 7,
-//     goal: 4,
-//     matchPlayed: 10,
-//     assist: 2,
-//     view: 40,
-//   },
-//   {
-//     id: 8,
-//     name: "Vakhtang Tvaltvadze",
-//     jersey: 8,
-//     goal: 3,
-//     matchPlayed: 11,
-//     assist: 1,
-//     view: 30,
-//   },
-//   {
-//     id: 9,
-//     name: "Davit Gelashvili",
-//     jersey: 9,
-//     goal: 5,
-//     matchPlayed: 12,
-//     assist: 2,
-//     view: 50,
-//   },
-//   {
-//     id: 10,
-//     name: "Lasha Beridze",
-//     jersey: 10,
-//     goal: 6,
-//     matchPlayed: 13,
-//     assist: 4,
-//     view: 65,
-//   },
-// ];
+const players: Player[] = [
+  {
+    id: 1,
+    name: "Giorgi Papava",
+    jersey: 1,
+    goal: 0,
+    matchPlayed: 5,
+    assist: 0,
+    view: 12,
+  },
+  {
+    id: 2,
+    name: "Levan Koberidze",
+    jersey: 2,
+    goal: 1,
+    matchPlayed: 7,
+    assist: 0,
+    view: 20,
+  },
+  {
+    id: 3,
+    name: "Nika Tchikovani",
+    jersey: 3,
+    goal: 0,
+    matchPlayed: 6,
+    assist: 2,
+    view: 18,
+  },
+  {
+    id: 4,
+    name: "Zurab Lobjanidze",
+    jersey: 4,
+    goal: 2,
+    matchPlayed: 8,
+    assist: 1,
+    view: 25,
+  },
+  {
+    id: 5,
+    name: "Irakli Shengelia",
+    jersey: 5,
+    goal: 1,
+    matchPlayed: 4,
+    assist: 0,
+    view: 14,
+  },
+  {
+    id: 6,
+    name: "Saba Khutsishvili",
+    jersey: 6,
+    goal: 0,
+    matchPlayed: 9,
+    assist: 3,
+    view: 27,
+  },
+  {
+    id: 7,
+    name: "Giorgi Kapanadze",
+    jersey: 7,
+    goal: 4,
+    matchPlayed: 10,
+    assist: 2,
+    view: 40,
+  },
+  {
+    id: 8,
+    name: "Vakhtang Tvaltvadze",
+    jersey: 8,
+    goal: 3,
+    matchPlayed: 11,
+    assist: 1,
+    view: 30,
+  },
+  {
+    id: 9,
+    name: "Davit Gelashvili",
+    jersey: 9,
+    goal: 5,
+    matchPlayed: 12,
+    assist: 2,
+    view: 50,
+  },
+  {
+    id: 10,
+    name: "Lasha Beridze",
+    jersey: 10,
+    goal: 6,
+    matchPlayed: 13,
+    assist: 4,
+    view: 65,
+  },
+];
 
-// const tournaments = [
-//   {
-//     id: 1,
-//     name: "gff",
-//     age: "u-19",
-//     teams: [
-//       {
-//         team: "იბერია 1999",
-//         played: 16,
-//         won: 13,
-//         drawn: 2,
-//         lost: 1,
-//         for: 41,
-//         against: 14,
-//         goalDifference: 27,
-//         points: 41,
-//       },
-//       {
-//         team: "დინამო თბილისი",
-//         played: 16,
-//         won: 12,
-//         drawn: 2,
-//         lost: 2,
-//         for: 47,
-//         against: 19,
-//         goalDifference: 28,
-//         points: 38,
-//       },
-//     ],
-//   },
-//   {
-//     id: 2,
-//     name: "league",
-//     age: "u-17",
-//     teams: [
-//       {
-//         team: "Inter",
-//         played: 6,
-//         won: 6,
-//         drawn: 0,
-//         lost: 0,
-//         for: 19,
-//         against: 7,
-//         goalDifference: 12,
-//         points: 18,
-//       },
-//     ],
-//   },
-// ];
+const tournaments = [
+  {
+    id: 1,
+    name: "gff",
+    age: "u-19",
+    teams: [
+      {
+        team: "იბერია 1999",
+        played: 16,
+        won: 13,
+        drawn: 2,
+        lost: 1,
+        for: 41,
+        against: 14,
+        goalDifference: 27,
+        points: 41,
+      },
+      {
+        team: "დინამო თბილისი",
+        played: 16,
+        won: 12,
+        drawn: 2,
+        lost: 2,
+        for: 47,
+        against: 19,
+        goalDifference: 28,
+        points: 38,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "league",
+    age: "u-17",
+    teams: [
+      {
+        team: "Inter",
+        played: 6,
+        won: 6,
+        drawn: 0,
+        lost: 0,
+        for: 19,
+        against: 7,
+        goalDifference: 12,
+        points: 18,
+      },
+    ],
+  },
+];
 
-// export { matches, news, players, tournaments };
+export { matches, news, players, tournaments };
