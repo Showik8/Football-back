03734879789455df433683b75c90b06ea645af47
Showik8 <shotaabashidze88@gmail.com>
2025-09-10 16:52:04 +0400

    working on routes

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import express from "express";
 import cors from "cors";
 import type { Request, Response } from "express";
-// import { VercelRequest, VercelResponse } from "@vercel/node";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 import matchesRouter from "./routes/mathcesRouter";
 import newsRouter from "./routes/newsRouter";
 import statisticRouter from "./routes/statisticRouter";
 import tournamentsRouter from "./routes/tournamentsRouter";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
-const allowedOrigins = [process.env.DEV_HOST!, process.env.PROD_HOST!];
+const allowedOrigins = [process.env.PROD_HOST!];
 
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 
@@ -27,7 +29,7 @@ app.use("/api/statistic", statisticRouter);
 
 app.use("/api/tournaments", tournamentsRouter);
 
-// export default (req: VercelRequest, res: VercelResponse) => app(req, res);
+export default (req: VercelRequest, res: VercelResponse) => app(req, res);
 
 app.listen(8888, () => {
   console.log("working");
